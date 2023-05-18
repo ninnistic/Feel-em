bdc197236cb435a4e08734960dcd8dcd6a5b23a9
ninnistic <parkninni@icloud.com>
2023-05-18 12:01:37 +0900

    Add : Feelogs에 Feelog/Mood 모델 추가 및 urls.py 경로 추가

--- server/feelogs/models.py
+++ server/feelogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from movies.models import Movie
 # Create your models here.
+
+class Mood(models.Model):
+    title = models.CharField(max_length=10)
+    image = models.CharField(max_length=100)
+class Feelog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='feelogs')
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE, related_name='mood_feelog')
+    
