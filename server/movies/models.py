30dd8a57654c7d5f7e32580f6c2d1dbef657cac3
ninnistic <parkninni@icloud.com>
2023-05-18 12:09:39 +0900

    Update : Movie 모델에 like_users 를 save_users로 변경

--- server/movies/models.py
+++ server/movies/models.py
@@ -13,4 +13,4 @@ class Movie(models.Model):
     popularity = models.IntegerField()
     vote_average = models.FloatField()
     genres = models.ManyToManyField(Genre, related_name='movies')
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+    save_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='save_movies')
