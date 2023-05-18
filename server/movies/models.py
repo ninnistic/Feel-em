f2074ddc70f01848ba4653a7e3bc2661cf49c8d7
ninnistic <parkninni@icloud.com>
2023-05-18 11:34:35 +0900

    Add : models.py에 Movie, Genre 모델 생성 및 urls.py 생성

--- server/movies/models.py
+++ server/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=500)
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=300)
+    release_date= models.DateTimeField()
+    popularity = models.IntegerField()
+    vote_average = models.FloatField()
+    genres = models.ManyToManyField(Genre, related_name='movies')
--- server/movies/urls.py
+++ server/movies/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    
+    # 영화 전체 조회 ('GET' : movies/)
+    # path('', views.movie_list, name='movie_list' ),
+    # 단일 영화 조회 ('GET' : movies/<movie_pk>)
+    # path('<movie_pk>', views.movie, name='movie'),
+    # 전체 장르 조회 ('GET' : movies/genres)
+    # path('genres', views.genres, name='genres')
 ]
