5ff29453ae28a8455b55653e935c9373d06aab2e
ninnistic <parkninni@icloud.com>
2023-05-18 12:06:36 +0900

    Add : User모델에 이달의 목표 추가

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    goal_of_month = models.IntegerField()
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
