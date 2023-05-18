3779a1466b9fb5d449a4014392ee15f6bcc302f4
신지원 <acd0825@naver.com>
2023-05-18 14:13:44 +0900

    Add: 영화 개봉일 시간대 제거한 데이터 추가(model 내 datetime field 수정)

--- server/movies/models.py
+++ server/movies/models.py
@@ -9,7 +9,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=500)
     overview = models.TextField()
     poster_path = models.CharField(max_length=300)
-    release_date= models.DateTimeField()
+    release_date= models.DateField()
     popularity = models.IntegerField()
     vote_average = models.FloatField()
     genres = models.ManyToManyField(Genre, related_name='movies')
