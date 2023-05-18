18464e5e8edbcf860ed1c4cb3d06c6ddd4923850
ninnistic <parkninni@icloud.com>
2023-05-18 16:14:14 +0900

    Setup : Vue 초기 설정 완료

---
+++ client/feelem/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+App.vue 입니다.
+<nav>
+  <router-link :to="{ name : 'AboutView'}">AboutView</router-link>
+</nav>
+<router-view />      
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+  
+    }
+  },
+  methods : {
+
+  }
+};
+</script>
+
+<style>
+
+</style>
