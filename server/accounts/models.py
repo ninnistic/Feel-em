43e4dfb1694b419ace97b97371d2936f13814f22
신지원 <acd0825@naver.com>
2023-05-19 16:59:06 +0900

    Update: 데이터 movie_id 추가

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from movies.models import Genre
 
 # # Create your models here.
 class User(AbstractUser):
     goal_of_month = models.IntegerField(default=1)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    # favorite_genre = models.ManyToManyField('Genre',related_name='user_genre')
+    favorite_genre = models.ManyToManyField(Genre, related_name='user_genre')
     is_superuser = models.BooleanField(default=False)
