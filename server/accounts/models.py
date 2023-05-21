421c2de371cfa796b6286758f268292c3966482c
Jay <acd0825@naver.com>
2023-05-21 14:55:57 +0900

    Update: feelog, user 생성 시 MTM field 필수로 지정되지 않도록 수정

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -5,6 +5,6 @@ from movies.models import Genre
 # # Create your models here.
 class User(AbstractUser):
     goal_of_month = models.IntegerField(default=1)
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
     favorite_genre = models.ManyToManyField(Genre, related_name='user_genre')
     is_superuser = models.BooleanField(default=False)
